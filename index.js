4171825c14d4999ec1adc713153198d3a6b4a6a5
Sebastian Pekarek <mail@sebbo.net>
2017-10-26 16:21:43 +0200

    Notification Cache

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express'), 
-      request = require('request'), 
-      multer  = require('multer'),
-      Pushover = require('pushover-notifications')
+const express = require('express');
+const multer = require('multer');
+const Pushover = require('pushover-notifications');
 
-      upload = multer({ dest: '/tmp/' }),
-      app = express(),
+const upload = multer({dest: '/tmp/'});
+const app = express();
 
-      p = new Pushover( {
-		user: process.env['PUSHOVER_USER'],
-		token: process.env['PUSHOVER_TOKEN'],
-	  }),
+const p = new Pushover({
+	user: process.env['PUSHOVER_USER'],
+	token: process.env['PUSHOVER_TOKEN']
+});
 
-	  emojis = {
-	  	"media.play": "▶️",
-	  	"media.pause": "⏸",
-	  	"media.resume": "⏯",
-	  	"media.stop": "⏹"
-	  };
+const lastMsg = {};
+const emojis = {
+	'media.play': '▶️',
+	'media.pause': '⏸',
+	'media.resume': '⏯',
+	'media.stop': '⏹'
+};
+
 
 if (!process.env['PUSHOVER_USER']) {
 	throw new Error('Unable to start: PUSHOVER_USER empty');
@@ -25,36 +26,50 @@ if (!process.env['PUSHOVER_TOKEN']) {
 	throw new Error('Unable to start: PUSHOVER_TOKEN empty');
 }
 
-app.get('/ping', function(req, res) {
+app.get('/ping', function (req, res) {
 	res.send('pong');
 });
 
-app.post('/', upload.single('thumb'), function(req, res, next) {
+app.post('/', upload.single('thumb'), function (req, res) {
 	let payload = JSON.parse(req.body.payload),
 		msg = {};
 
 	res.send(201)
-	if(!emojis[ payload.event ]) {
+	if (!emojis[payload.event]) {
 		return;
 	}
 
+	if (
+		lastMsg[payload.Account.id] &&
+		lastMsg[payload.Account.id].e === payload.event &&
+		lastMsg[payload.Account.id].k === payload.Metadata.key
+	) {
+		// Notification already sent!
+		return;
+	}
 
-	msg.title = emojis[ payload.event ] + " " + payload.Account.title;
+	lastMsg[payload.Account.id] = {
+		e: payload.event,
+		k: payload.Metadata.key
+	};
+
+
+	msg.title = emojis[payload.event] + ' ' + payload.Account.title;
 
 	// Movies
-	if(payload.Metadata.librarySectionType === 'movie') {
+	if (payload.Metadata.librarySectionType === 'movie') {
 		msg.message = payload.Metadata.title;
 	}
 
 	// TV Shows
-	else if(payload.Metadata.librarySectionType === 'show') {
-		msg.message = payload.Metadata.grandparentTitle + 
-						" S" + (payload.Metadata.parentIndex < 10 ? "0" : "") + payload.Metadata.parentIndex + 
-						"E" + payload.Metadata.index;
+	else if (payload.Metadata.librarySectionType === 'show') {
+		msg.message = payload.Metadata.grandparentTitle +
+			' S' + (payload.Metadata.parentIndex < 10 ? '0' : '') + payload.Metadata.parentIndex +
+			'E' + payload.Metadata.index;
 	}
 
 	// Trailer
-	else if(payload.Metadata.cinemaTrailer) {
+	else if (payload.Metadata.cinemaTrailer) {
 		msg.message = 'Trailer: ' + payload.Metadata.title;
 	}
 
@@ -62,13 +77,13 @@ app.post('/', upload.single('thumb'), function(req, res, next) {
 		return;
 	}
 
-	msg.url = "https://app.plex.tv/web/app#!/server/" + 
-				payload.Server.uuid + "/details/" + 
-				encodeURIComponent(payload.Metadata.key);
-	msg.url_title = "View details";
+	msg.url = 'https://app.plex.tv/web/app#!/server/' +
+		payload.Server.uuid + '/details/' +
+		encodeURIComponent(payload.Metadata.key);
+	msg.url_title = 'View details';
 
-	p.send( msg, function(err, result) {
-		if( err ) {
+	p.send(msg, function (err, result) {
+		if (err) {
 			console.log('Pushover Error:', err);
 			res.status(500).send(err);
 			return;
