7067571e49d6809be97e19d2285daa72ebc2a19a
Sebastian Pekarek <mail@sebbo.net>
2018-12-02 15:03:51 +0100

    🔒 Security Check

--- index.js
+++ index.js
@@ -6,91 +6,91 @@ const upload = multer({dest: '/tmp/'});
 const app = express();
 
 const p = new Pushover({
-	user: process.env['PUSHOVER_USER'],
-	token: process.env['PUSHOVER_TOKEN']
+    user: process.env['PUSHOVER_USER'],
+    token: process.env['PUSHOVER_TOKEN']
 });
 
 const lastMsg = {};
 const emojis = {
-	'media.play': '▶️',
-	'media.pause': '⏸',
-	'media.resume': '⏯',
-	'media.stop': '⏹'
+    'media.play': '▶️',
+    'media.pause': '⏸',
+    'media.resume': '⏯',
+    'media.stop': '⏹'
 };
 
 
 if (!process.env['PUSHOVER_USER']) {
-	throw new Error('Unable to start: PUSHOVER_USER empty');
+    throw new Error('Unable to start: PUSHOVER_USER empty');
 }
 if (!process.env['PUSHOVER_TOKEN']) {
-	throw new Error('Unable to start: PUSHOVER_TOKEN empty');
+    throw new Error('Unable to start: PUSHOVER_TOKEN empty');
 }
 
 app.get('/ping', function (req, res) {
-	res.send('pong');
+    res.send('pong');
 });
 
 app.post('/', upload.single('thumb'), function (req, res) {
-	let payload = JSON.parse(req.body.payload),
-		msg = {};
+    let payload = JSON.parse(req.body.payload),
+        msg = {};
 
-	res.send(201)
-	if (!emojis[payload.event]) {
-		return;
-	}
+    res.send(201);
+    if (!emojis[payload.event]) {
+        return;
+    }
 
-	if (
-		lastMsg[payload.Account.id] &&
-		lastMsg[payload.Account.id].e === payload.event &&
-		lastMsg[payload.Account.id].k === payload.Metadata.key
-	) {
-		// Notification already sent!
-		return;
-	}
+    if (
+        lastMsg[payload.Account.id] &&
+        lastMsg[payload.Account.id].e === payload.event &&
+        lastMsg[payload.Account.id].k === payload.Metadata.key
+    ) {
+        // Notification already sent!
+        return;
+    }
 
-	lastMsg[payload.Account.id] = {
-		e: payload.event,
-		k: payload.Metadata.key
-	};
+    lastMsg[payload.Account.id] = {
+        e: payload.event,
+        k: payload.Metadata.key
+    };
 
 
-	msg.title = emojis[payload.event] + ' ' + payload.Account.title;
+    msg.title = emojis[payload.event] + ' ' + payload.Account.title;
 
-	// Movies
-	if (payload.Metadata.librarySectionType === 'movie') {
-		msg.message = payload.Metadata.title;
-	}
+    // Movies
+    if (payload.Metadata.librarySectionType === 'movie') {
+        msg.message = payload.Metadata.title;
+    }
 
-	// TV Shows
-	else if (payload.Metadata.librarySectionType === 'show') {
-		msg.message = payload.Metadata.grandparentTitle +
-			' S' + (payload.Metadata.parentIndex < 10 ? '0' : '') + payload.Metadata.parentIndex +
-			'E' + payload.Metadata.index;
-	}
+    // TV Shows
+    else if (payload.Metadata.librarySectionType === 'show') {
+        msg.message = payload.Metadata.grandparentTitle +
+            ' S' + (payload.Metadata.parentIndex < 10 ? '0' : '') + payload.Metadata.parentIndex +
+            'E' + payload.Metadata.index;
+    }
 
-	// Trailer
-	else if (payload.Metadata.cinemaTrailer) {
-		msg.message = 'Trailer: ' + payload.Metadata.title;
-	}
+    // Trailer
+    else if (payload.Metadata.cinemaTrailer) {
+        msg.message = 'Trailer: ' + payload.Metadata.title;
+    }
 
-	else {
-		return;
-	}
+    else {
+        return;
+    }
 
-	msg.url = 'https://app.plex.tv/web/app#!/server/' +
-		payload.Server.uuid + '/details/' +
-		encodeURIComponent(payload.Metadata.key);
-	msg.url_title = 'View details';
+    msg.url = 'https://app.plex.tv/web/app#!/server/' +
+        payload.Server.uuid + '/details/' +
+        encodeURIComponent(payload.Metadata.key);
+    msg.url_title = 'View details';
 
-	p.send(msg, function (err, result) {
-		if (err) {
-			console.log('Pushover Error:', err);
-			res.status(500).send(err);
-			return;
-		}
+    p.send(msg, function (err) {
+        if (err) {
+            console.log('Pushover Error:', err);
+            res.status(500).send(err);
+            return;
+        }
 
-		res.sendStatus(201);
-	});
+        res.sendStatus(201);
+    });
 });
 
 app.listen(process.env.PORT || 8888);
